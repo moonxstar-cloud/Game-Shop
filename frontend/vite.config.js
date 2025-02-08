0d18881f9876493a4505f99734bc7efcf252f79a
MoonXstar <sunrisingly47@gmail.com>
2025-02-08 12:25:26 +0500

    push changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,7 @@ import path from 'path';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // Adjust if needed for proper routing
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
