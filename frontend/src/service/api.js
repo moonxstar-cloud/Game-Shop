4bb205f248f1bd475b81ea000d85c959955186b0
MoonXstar <sunrisingly47@gmail.com>
2025-02-13 15:16:05 +0500

    Your descriptive commit message

--- frontend/src/service/api.js
+++ frontend/src/service/api.js
@@ -1,10 +1,10 @@
 // src/services/api.js
 import axios from 'axios';
-import API_BASE_URL from '../api';
+
 
 const createApiInstance = () => {
   const api = axios.create({
-    baseURL:  API_BASE_URL,
+    baseURL: process.env.REACT_APP_BACKEND_URL,
   });
 
   // Add a request interceptor to add the token
