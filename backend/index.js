139a057761446609d240e67d2f398f6c841cc857
MoonXstar <sunrisingly47@gmail.com>
2025-02-13 13:49:25 +0500

    Your descriptive commit message

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:5173', // Your frontend URL
+  origin: 'https://game-shop-frontend-ten.vercel.app', // Your frontend URL
   credentials: true
 }));
 app.use(express.json());
--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,4 +1,4 @@
 
-const API_BASE_URL = 'http://localhost:3000';
+const API_BASE_URL = 'https://game-shop-mern.vercel.app';
 
 export default API_BASE_URL;
